a2dd5a5ca09145954f8ba1349bf2728d8e75a7c5
phil <phil@wreckedgames.com>
2023-08-23 20:58:13 -0700

    Updating template definition

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import shouldUpdate from './utils/shouldUpdate'
 import isFn from './utils/isFn'
 
 type Reducer<TStore, TAction> = (store: TStore, action: TAction) => TStore
+type ReducerAction<R extends Reducer<any, any>> = R extends Reducer<any, infer A> ? A : never;
 
-type Listener<TStore> = {
-  mapState: (store: TStore) => TStore
-  updater: React.Dispatch<React.SetStateAction<TStore>>
+type Listener = {
+  mapState: (store: any) => any
+  updater: React.Dispatch<React.SetStateAction<any>>
 }
 
 const createStore = <TStore, R extends Reducer<TStore, any>>(
@@ -14,11 +15,11 @@ const createStore = <TStore, R extends Reducer<TStore, any>>(
   reducer?: R
 ) => {
   let store: TStore = initialStore
-  const listeners = new Set<Listener<TStore>>()
+  const listeners = new Set<Listener>()
 
   const getStore = () => store
 
-  const dispatch = (action: TStore | ((prev: TStore) => TStore) | ((prev: TStore, action: any) => TStore)) => {
+  const dispatch = (action: TStore | ((prev: TStore) => TStore) | ReducerAction<R>) => {
     const oldStore = store
 
     if (reducer) {
@@ -35,7 +36,25 @@ const createStore = <TStore, R extends Reducer<TStore, any>>(
     })
   }
 
-  const useStore = (mapState = (s: TStore) => s) => {
+  const pickStore = <K extends keyof TStore>(mapState:((s: TStore) => Pick<TStore, K>) = (s) => s) => {
+    const [, updater] = useState()
+
+    useEffect(() => {
+      const listener = {
+        updater,
+        mapState,
+      }
+
+      listeners.add(listener)
+      return () => {
+        listeners.delete(listener)
+      }
+    }, [mapState])
+
+    return mapState(store)
+  }
+  
+  const useStore = (mapState:((s: TStore) => TStore) = (s) => s) => {
     const [, updater] = useState<TStore>()
 
     useEffect(() => {
@@ -53,7 +72,7 @@ const createStore = <TStore, R extends Reducer<TStore, any>>(
     return mapState(store)
   }
 
-  return [useStore, dispatch, getStore] as const
+  return [useStore, dispatch, getStore, pickStore] as const
 }
 
 export default createStore
